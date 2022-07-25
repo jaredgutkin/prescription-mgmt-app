3aa7309234e06a06b76d4216a64d206c29041d17
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-25 11:35:30 -0500

    app.js and home set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
+import { Home } from './pages/Home';
+import { ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        prescriptions: {
+          merge(existing, incoming){
+            return incoming
+          }
+        },
+      }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache
+})
+
 function App() {
   return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+          </div>
+        </Router>
 
-    <Header />
+      </ApolloProvider>
+    </>
+
   );
 }
 
